301f8e858ec204af0e230934415ab959b6b605a2
Mohammad <mohammad.yi2020@gmail.com>
2022-12-11 14:54:29 +0330

    Added Hi

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Learning_Projects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi..");
+        }
+    }
+}
